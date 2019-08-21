882ad6937ccb7505bd9be178cffb98b4f3d48926
ffaian <ffaian@gmail.com>
2019-08-21 11:46:25 -0400

    August 21, 2019 - v4

---
+++ srv/router/index.js
@@ -0,0 +1,7 @@
+/*eslint-env node, es6 */
+"use strict";
+
+module.exports = (app, server) => {
+	app.use("/node", require("./routes/myNode")());
+	app.use("/node/ex2", require("./routes/ex2")());
+};
--- xsjs/server.js
+++ xsjs/server.js
@@ -24,6 +24,15 @@ try {
 	console.log("[WARN]", err.message);
 }
 
+// Add SQLCC
+try {
+	options.hana.sqlcc  = xsenv.getServices({
+		"xsjs.sqlcc_config": "CROSS_SCHEMA_SFLIGHT_00"
+	});
+} catch (err) {
+	console.log("[WARN]", err.message);
+}
+
 // start server
 xsjs(options).listen(port);
 
