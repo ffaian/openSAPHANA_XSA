907a9d3531180ae2bfffacc7262a10032f855ea5
ffaian <ffaian@gmail.com>
2019-08-22 16:52:19 -0400

    August 22, 2019 - v5

--- srv/router/index.js
+++ srv/router/index.js
@@ -4,4 +4,13 @@
 module.exports = (app, server) => {
 	app.use("/node", require("./routes/myNode")());
 	app.use("/node/ex2", require("./routes/ex2")());
+	app.use("/node/excAsync",
+		require("./routes/exerciseAsync")(server));
+	app.use("/node/JavaScriptBasics",
+		require("./routes/JavaScriptBasics")());
+	app.use("/node/textBundle",
+		require("./routes/textBundle")());
+	app.use("/node/excel", require("./routes/excel")());
+	app.use("/node/xml", require("./routes/xml")());
+	app.use("/node/zip", require("./routes/zip")());
 };
